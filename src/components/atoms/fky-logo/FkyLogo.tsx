f7b4ac263e9a78cc9327416a25b1e5d2979c18ec
Ricky <ricardo.vadala@pressganey.com>
2023-01-27 11:21:24 -0500

    tweaks

--- src/components/atoms/fky-logo/FkyLogo.tsx
+++ src/components/atoms/fky-logo/FkyLogo.tsx
@@ -1,13 +1,21 @@
 import './FkyLogo.css'
 import {useLocation, useNavigate} from "react-router-dom";
 import chevron from '../../../assets/chevron.svg'
+import {useAppDispatch} from "../../../store/hooks";
+import {onCommandSelected} from "../../../store/slices/prompts";
 
 export const FkyLogo = () => {
     let navigate = useNavigate();
     const location = useLocation();
+    const dispatch = useAppDispatch()
+
+    const goHome = () => {
+        dispatch(onCommandSelected(''))
+        navigate('/')
+    }
 
     return (
-        <div className={'fky-logo'} onClick={() => navigate('/')}>
+        <div className={'fky-logo'} onClick={goHome}>
             <img src="/faktory.svg" alt="Faktory logo" className={'fky-logo_img'}/>
             {location.pathname.includes('editor') && <img src={chevron} alt="" className={'fky-logo_chevron'}/>}
         </div>
